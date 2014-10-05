b4e36bc49b3f06d3af2cdb22b6883be246b30dfe
Sean Kallungal <sean.kallungal@gmail.com>
2014-10-05 07:20:44 -0400

    added splash screen and logo b/c swag

---
+++ hackmit20142/app/src/main/java/com/mittens/hackmit2014/splash.java
@@ -0,0 +1,54 @@
+package com.mittens.hackmit2014;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        /*
+        Button button = (Button) findViewById(R.id.startbutton).setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), MapsActivity.class);
+                startActivity(myIntent);
+            }
+        });*/
+    }
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(view.getContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
